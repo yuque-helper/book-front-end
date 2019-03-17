194186bb5c792b8e24d737faa183c341041c52dd
cctv1005s <384113872@qq.com>
2019-03-17 20:02:22 +0800

    feat: 添加右侧toc

--- src/coms/body/index.js
+++ src/coms/body/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import hljs from 'highlight.js';
 import $ from 'jquery';
+import _ from 'lodash';
+import qs from 'query-string';
+import hljs from 'highlight.js';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import inViewport from 'in-viewport';
+import {withRouter} from 'dva/router';
 
 import {copyButton} from '../../util/copy';
 
 import 'highlight.js/styles/github.css'
 import styles from './index.less';
+
+const MAX_LEVEL = 4;
 
 class Body extends React.Component{
   static propTypes = {
@@ -16,7 +23,18 @@ class Body extends React.Component{
       slug: PropTypes.oneOfType([
         PropTypes.number, PropTypes.string
       ])
+    }),
+    location: PropTypes.shape({
+      search: PropTypes.string
+    }),
+    history: PropTypes.shape({
+      push: PropTypes.func
     })
+  }
+
+  state = {
+    toc: [],
+    activeTocId: null
   }
 
   componentDidUpdate = (nextProps) => {
@@ -25,7 +43,106 @@ class Body extends React.Component{
         hljs.highlightBlock(block);
         copyButton(block);
       });
+
+      this.initDocToc();
     }
+  }
+
+  componentDidMount = () => {
+    const search = _.get(this, 'props.location.search');
+
+    // 锚点恢复
+    if(search) {
+      const query = qs.parse(search);
+      const anchor = query.anchor;
+      if(anchor) {
+        this.setState({
+          activeTocId: anchor
+        });
+
+        // FIXME: 锚点恢复, 不一定能等到dom生成了才执行
+        setTimeout(this.toView(anchor), 100);
+      }
+    }
+  }
+
+  // 初始化右侧toc
+  initDocToc = () => {
+    const div = $('#yuque-book-container');
+
+    const isaAnchor = (ele) => {
+      return ele.find('a').length && ele.text().trim() !== ''; 
+    }
+
+    /**
+     * 
+     * @param {element} element 需要遍历的元素
+     * @param {number} level h级别
+     * @param {number} depth 遍历深度
+     */
+    const findChildren = (element, level, depth) => {
+      if(level >= MAX_LEVEL) {
+        return [];
+      }
+
+      const eles = $(element).find(`h${level}[id]`);
+      const tree = [];
+
+      for(let ele of eles) {
+        ele = $(ele);
+        if(isaAnchor(ele)) {
+          tree.push({
+            ele,
+            depth,
+            title: ele.text(),
+            id: ele.attr('id'),
+            children: findChildren(ele, level + 1, depth + 1),
+            level: level
+          })
+        }
+      }
+
+      if(!tree.length) {
+        return findChildren(element, level + 1, depth);
+      }
+
+      return tree;
+    }
+
+    let tree = findChildren(div, 1, 0) || [];
+
+    this.setState({
+      toc: _.flattenDeep(tree)
+    }, this.onScroll);
+  }
+
+  onScroll = () => {
+    const {toc} = this.state;
+    for(let item of toc) {
+      const ele = _.get(item,'ele[0]');
+      if(!ele) {
+        continue;
+      }
+
+      if(inViewport(ele)){
+        this.setState({
+          activeTocId: item.id
+        });
+
+        return;
+      }
+    }
+  }
+
+  toView = (id) => {
+    return () => {
+      document.getElementById(id).scrollIntoView(true);
+      this.props.history.push({
+        search: qs.stringify({
+          anchor: id
+        })
+      })
+    };
   }
 
   render(){
@@ -34,15 +151,43 @@ class Body extends React.Component{
       return null;
     }
 
+    const {toc, activeTocId} = this.state;
+
     return (
-      <div className={styles.container + " lake-engine-view"} >
-        <div className={styles['doc-container'] + ' typo'}>
+      <div onScroll={this.onScroll} className={styles.container + " lake-engine-view"} >
+        <div className={styles['doc-container'] + ' typo'} id="yuque-book-container">
           <h1>{doc.title}</h1>
           <div dangerouslySetInnerHTML={{__html: doc.body_html}} ></div>
+          <div className={styles['doc-wrapper']}>
+          <div className={styles['doc-toc']}>
+            {
+              toc.map(item => {
+                return (
+                  <div 
+                    className={
+                      classnames(
+                        styles['doc-item'], 
+                        `doc-link-${item.depth}`, 
+                        {
+                          [styles['doc-item-active']]: activeTocId === item.id
+                        }
+                      )
+                    }
+                    onClick={this.toView(item.id)}
+                  >
+                    {
+                      item.title
+                    }
+                  </div>
+                )
+              })
+            }
+          </div>   
+          </div>       
         </div>
       </div>
     )
   }
 }
 
-export default Body;
+export default withRouter(Body);
